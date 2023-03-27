1d2801b4d0ce13e3515507c00c17268824993631
Gyanthakur <98226958+Gyanthakur@users.noreply.github.com>
2023-03-27 16:32:34 +0530

    Fourth commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,37 +1,38 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom';
-import styled from 'styled-components'
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 import logo from "../assets/logo.png";
-const Container = styled.div`
 
+
+const StyledHeader = styled.header`
   padding: 0 4rem;
-  .logo{
-      img{
-        height: 5rem;
-      }
+  .logo {
+    img {
+      height: 5rem;
+    }
   }
-  button{
-   padding: 0.5rem 1rem;
-   background-color: #e50914;
-   border: none;
-   cursor: pointer;
-   color: white;
-   border-radius: 0.2rem;
-   font-weight: bolder;
-   font-size: 1.05rem;
+  button {
+    padding: 0.5rem 1rem;
+    background-color: #e50914;
+    border: none;
+    cursor: pointer;
+    color: white;
+    border-radius: 0.2rem;
+    font-weight: bolder;
+    font-size: 1.05rem;
   }
 `;
 
-const Header = (props) => {
-    const  navigate =useNavigate();
+export default function Header(props) {
+  const navigate = useNavigate();
   return (
-    <Container>
-        <div className="logo">
-            <img src={logo} alt="" />
-        </div>
-        <button onClick={()=>navigate(props.login ? "/login" : "/signup")}>{props.login ? "Log In" : "Sign In"}</button>
-    </Container>
-  )
+    <StyledHeader className="flex a-center j-between">
+      <div className="logo">
+        <img src={logo} alt="logo" />
+      </div>
+      <button onClick={() => navigate(props.login ? "/login" : "/signup")}>
+        {props.login ? "Log In" : "Sign In"}
+      </button>
+    </StyledHeader>
+  );
 }
-
-export default Header
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,26 +1,96 @@
-import React from 'react'
+
+import React, {useState} from 'react'
 import styled from 'styled-components';
 import BackgroundImages from '../components/BackgroundImages'
 import Header from '../components/Header'
-
+import {firebaseAuth} from "../utils/firebase-config"
+import { createUserWithEmailAndPassword , onAuthStateChanged} from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
 const Container = styled.div`
-    position: relative;
-    top:0;
-    left:0;
-    background-color: rgba(0,0,0,0.5);
-    width:100vw;
-    height:100vh;
-    display:grid;
-    grid-template-rows:15vh 85vh;
-    
+  position: relative;
+  .content {
+    position: absolute;
+    top: 0;
+    left: 0;
+    background-color: rgba(0, 0, 0, 0.5);
+    height: 100vh;
+    width: 100vw;
+    display: grid;
+    grid-template-rows: 15vh 85vh;
+    .body {
+      gap: 1rem;
+      .text {
+        gap: 1rem;
+        text-align: center;
+        font-size: 1.3rem;
+        h1 {
+          padding: 0 25rem;
+        }
+      }
+      .form {
+        display: grid;
+         grid-template-columns: ${({ showPassword }) =>
+           showPassword ? "1fr 1fr" : "2fr 1fr"}; 
+        width: 60%;
+        input {
+          color: black;
+          border: none;
+          padding: 1rem;
+          font-size: 1.2rem;
+          border: 1px solid black;
+          &:focus {
+            outline: none;
+          }
+        }
+        button {
+          padding: 0.5rem 1rem;
+          background-color: #e50914;
+          border: none;
+          cursor: pointer;
+          color: white;
+          font-weight: bolder;
+          font-size: 1.05rem;
+        }
+      }
+      button {
+        padding: 0.5rem 1rem;
+        background-color: #e50914;
+        border: none;
+        cursor: pointer;
+        color: white;
+        border-radius: 0.2rem;
+        font-weight: bolder;
+        font-size: 1.05rem;
+      }
+    }
+  }
 `;
 
 const Signup = () => {
+    const navigate = useNavigate();
+    const [showPassword, setShowPassword] = useState(false)
+    const [formValues, setFormValues] = useState({
+        email:"",
+        password:"",
+    })
+
+    const handleSignIn = async () => {
+        try {
+          const { email, password } = formValues;
+          await createUserWithEmailAndPassword(firebaseAuth,email,password)
+        } catch (err) {
+          console.log(err);
+        }
+      };
+
+      onAuthStateChanged(firebaseAuth, (currentUser) => {
+        if (currentUser) navigate("/");
+      });
   return (
-  <Container>
+  <Container showPassword={showPassword}>
     <BackgroundImages/>
     <div className="content">
-    <Header/>
+    <Header login />
     <div className="body flex column a-center j-center">
         <div className="text flex column">
             <h1>Unlimited movies, TV shows and more</h1>
@@ -30,11 +100,25 @@ const Signup = () => {
             </h6>
         </div>
         <div className="form">
-            <input type="email" placeholder='email Address' name='email' />
-            <input type="password" placeholder='Password' name='password' />
-            <button>Get Started</button>
+            <input type="email" placeholder='email Address' name='email' value={formValues.email} 
+            onChange={(e)=>setFormValues({...formValues,[e.target.name]:e.target.value,})} />
+            {
+                showPassword && (
+                    <input type="password" placeholder='Password' name='password'
+                    value={formValues.password} 
+                    onChange={(e)=>setFormValues({...formValues,[e.target.name]:e.target.value,})} />
+
+                )
+            }
+            {
+                !showPassword && (
+                    <button onClick={()=>setShowPassword(true)}>Get Started</button>
+
+                )
+            }
+
         </div>
-        <button>Login</button>
+        <button onClick={handleSignIn}>Sign up</button>
     </div>
     </div>
   </Container>
